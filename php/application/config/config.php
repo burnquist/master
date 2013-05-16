e43285b2c6eb947cc8128d035a912bf30540b279
burnquist <burnquist73@yahoo.com>
2013-05-16 23:12:34 +0700

    Update config.php

--- php/application/config/config.php
+++ php/application/config/config.php
@@ -18,7 +18,7 @@
 <<<<<<< HEAD
 //$config['base_url']	= '';
 =======
-//$config['base_url']	= 'http://bambang-priyatna.rhcloud.com';
+$config['base_url']	= 'http://bambang-priyatna.rhcloud.com';
 >>>>>>> f09a466f0fd7e4ce3b33ded10c07bb6d34f79d8f
 =======
 //$config['base_url']	= 'http://bambang-priyatna.rhcloud.com';
