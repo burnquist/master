45b1372ad0b5ffcf58c4a82fede7b72d6ebd1753
Bambang Priyatna <burnquist73@yahoo.com>
2013-05-25 17:00:35 +0700

    Update database.php

--- php/application/config/database.php
+++ php/application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = '';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['hostname'] = 'https://bambang-priyatna.rhcloud.com';
+$db['default']['username'] = 'adminPujBjPn';
+$db['default']['password'] = 'm9RXer2V2gjj';
+$db['default']['database'] = 'burn';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
