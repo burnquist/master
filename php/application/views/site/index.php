dabc56479d614bcd313424b6583ac143e025103c
Bambang Priyatna <burnquist73@yahoo.com>
2013-05-25 08:47:28 +0700

    Update index.php

--- php/application/views/site/index.php
+++ php/application/views/site/index.php
@@ -3,6 +3,16 @@
 <head>
   <title>Home - Home Page | monsterweb</title>
   <meta charset="utf-8">
+  <div id="fb-root"></div>
+<script>(function(d, s, id) {
+var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/id_ID/all.js#xfbml=1&appId=605601616134280";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+
   	<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDY0kkJiTPVd2U7aTOAwhc9ySH6oHxOIYM&sensor=false">
 </script>
 
